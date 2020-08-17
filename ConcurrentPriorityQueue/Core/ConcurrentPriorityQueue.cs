200f6b1766ef9ed9ea3a0c5036af270452661c4c
Ralf Jänike <jaenike@ferrari-electronic.de>
2020-08-17 08:22:20 +0200

    remove CSharpFunctionalExtensions from PriorityQueue

--- ConcurrentPriorityQueue/Core/ConcurrentPriorityQueue.cs
+++ ConcurrentPriorityQueue/Core/ConcurrentPriorityQueue.cs
@@ -2,36 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using CSharpFunctionalExtensions;
 
 namespace ConcurrentPriorityQueue.Core
 {
-    public class ConcurrentPriorityQueue<T, TP> : IConcurrentPriorityQueue<T, TP> 
-	    where T : IHavePriority<TP>
-		where TP : IEquatable<TP>, IComparable<TP>
+    public class ConcurrentPriorityQueue<T, TP> : IConcurrentPriorityQueue<T, TP>
+        where T : IHavePriority<TP>
+        where TP : IEquatable<TP>, IComparable<TP>
     {
         private readonly Dictionary<TP, Queue<T>> _internalQueues;
         private readonly object _syncRoot = new object();
-	    private readonly int _capacity;
+        private readonly int _capacity;
 
-        public int Count => _internalQueues.Count == 0 ? _internalQueues.Count : 
-	        _internalQueues.Values.Select(q => q.Count).Aggregate((a, b) => a + b);
         public int Capacity
         {
             get { return _capacity; }
         }
+        public int Count => _internalQueues.Count == 0 ? _internalQueues.Count :
+            _internalQueues.Values.Select(q => q.Count).Aggregate((a, b) => a + b);
 
         public bool IsSynchronized => true;
 
         public object SyncRoot => _syncRoot;
 
-		public ConcurrentPriorityQueue(int capacity = 0)
-		{
-			_internalQueues = new Dictionary<TP, Queue<T>>();
-			_capacity = capacity;
-		}
+        public ConcurrentPriorityQueue(int capacity = 0)
+        {
+            _internalQueues = new Dictionary<TP, Queue<T>>();
+            _capacity = capacity;
+        }
 
-		public void CopyTo(T[] array, int index)
+        public void CopyTo(T[] array, int index)
         {
             var itemsArray = ToArray();
             lock (_syncRoot)
@@ -39,7 +38,7 @@ namespace ConcurrentPriorityQueue.Core
         }
 
         public void CopyTo(Array array, int index) => CopyTo((T[])array, index);
-        
+
         public T[] ToArray()
         {
             lock (_syncRoot)
@@ -54,79 +53,84 @@ namespace ConcurrentPriorityQueue.Core
             }
         }
 
-        public bool TryAdd(T item) => Enqueue(item).IsSuccess;
+        public bool TryAdd(T item)
+        {
+            return Enqueue(item);
+        }
 
         public bool TryTake(out T item)
-        {           
-            var result = Dequeue();
+        {
+            var qi = Dequeue();
             lock (_syncRoot)
             {
                 item = default(T);
-                if (result.IsFailure)
+                if (qi == null)
                     return false;
 
-                item = result.Value;
+                item = qi;
                 return true;
-            }           
+            }
         }
 
         public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)ToArray()).GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public Result Enqueue(T item)
+        public bool Enqueue(T item)
         {
-			lock (_syncRoot) return AddOrUpdate(item);      
+            lock (_syncRoot) return AddOrUpdate(item);
         }
 
-        public Result<T> Dequeue()
-        {
-            lock (_syncRoot)
-            {
-                return GetNextQueue()
-                    .OnSuccess(q => q.Dequeue())
-                    .OnFailure(err => Result.Fail(err));
-            }
-        }
-
-        public Result<T> Peek()
+        public T Dequeue()
         {
             lock (_syncRoot)
             {
-                return GetNextQueue()
-                    .OnSuccess(q => q.Peek())
-                    .OnFailure(err => Result.Fail(err));
+                var q = GetNextQueue();
+                if (q != null)
+                    return q.Dequeue();
+                return default(T);
             }
         }
 
-        private Result<Queue<T>> GetNextQueue()
+        public T Peek()
+        {
+            lock (_syncRoot)
+            {
+                var q = GetNextQueue();
+                if (q != null)
+                    return q.Peek();
+                return default(T);
+            }
+        }
+
+        private Queue<T> GetNextQueue()
         {
             foreach (var queue in _internalQueues.OrderBy(q => q.Key).Select(q => q.Value))
             {
                 try
                 {
                     queue.Peek();
-                    return Result.Ok(queue);
+                    return queue;
                 }
                 catch (Exception) { }
             }
 
-            return Result.Fail<Queue<T>>("Could not find a queue with items.");
+            return null;
         }
 
-	    private Result AddOrUpdate(T item)
-	    {
-		    if (!_internalQueues.ContainsKey(item.Priority))
-		    {
-			    if (IsAtMaxCapacity())
-				    return Result.Fail("Reached max capacity.");
-			    _internalQueues.Add(item.Priority, new Queue<T>());
-			}
+        private bool AddOrUpdate(T item)
+        {
+            if (!_internalQueues.ContainsKey(item.Priority))
+            {
+                if (IsAtMaxCapacity())
+                    return false;
+                _internalQueues.Add(item.Priority, new Queue<T>());
+            }
 
-			_internalQueues[item.Priority].Enqueue(item);
-		    return Result.Ok();
-	    }
+            _internalQueues[item.Priority].Enqueue(item);
+            return true;
+        }
 
-	    private bool IsAtMaxCapacity() => _capacity != 0 && Count == _capacity;
+        private bool IsAtMaxCapacity() => _capacity != 0 && Count == _capacity;
     }
 }
